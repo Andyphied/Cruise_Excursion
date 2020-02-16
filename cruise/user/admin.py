86b24f8d41af1fee8ec8b169d65c20e7543b63a9
Philip Nnamdi Philip <philipandy2@gmail.com>
2020-02-16 13:58:06 +0100

    Update admin.py

--- cruise/user/admin.py
+++ cruise/user/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from .models import User
-#from customauth.models import User
+
 
 
 class UserCreationForm(forms.ModelForm):
